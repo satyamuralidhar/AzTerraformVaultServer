29f64d841ed0efab536fa6366bc4a536d41773cd
satya <muralidhar.peddireddi@gmail.com>
2023-01-06 19:59:52 +0530

    def

---
+++ main.tf
@@ -0,0 +1,184 @@
+provider "azurerm" {
+}
+
+resource "azurerm_resource_group" "rsg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+
+  tags {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vault-vnet"
+  location            = azurerm_resource_group.rsg.location
+  resource_group_name = azurerm_resource_group.rsg.name
+  address_space       = [var.address_space]
+
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vault-subnet"
+  resource_group_name  = azurerm_resource_group.rsg.name
+  virtual_network_name = azurerm_virtual_network.rsg.name
+  address_prefixes     = [var.subnet_prefixes]
+}
+
+
+resource "azurerm_public_ip" "vault-pip" {
+  name                         = "vault-public-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.rsg.name}"
+  public_ip_address_allocation = "static"
+  domain_name_label            = "${var.resource_group_name}-ssh"
+
+  tags {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_group" "vault-nsg" {
+  name                = "vault-ssh"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rsg.name}"
+}
+
+resource "azurerm_network_security_rule" "ssh_access" {
+  name                        = "ssh-access-rule"
+  network_security_group_name = "${azurerm_network_security_group.vault-nsg.name}"
+  direction                   = "Inbound"
+  access                      = "Allow"
+  priority                    = 200
+  source_address_prefix       = "*"
+  source_port_range           = "*"
+  destination_address_prefix  = "${azurerm_network_interface.vault-nic.private_ip_address}"
+  destination_port_range      = "22"
+  protocol                    = "TCP"
+  resource_group_name         = "${azurerm_resource_group.rsg.name}"
+}
+
+resource "azurerm_network_security_rule" "ssh_access_vault_demo" {
+  name                        = "allow-vault-demo-ssh"
+  direction                   = "Inbound"
+  access                      = "Allow"
+  priority                    = 210
+  source_address_prefix       = "*"
+  source_port_range           = "*"
+  destination_address_prefix  = "${azurerm_network_interface.vault-nic.private_ip_address}"
+  destination_port_range      = "22"
+  protocol                    = "Tcp"
+  resource_group_name         = "${azurerm_resource_group.rsg.name}"
+  network_security_group_name =  azurerm_network_security_group.vault-nsg.name
+}
+
+resource "azurerm_network_interface" "vault-nic" {
+  name                      = "vault-demo-nic"
+  location                  = "${var.location}"
+  resource_group_name       = "${azurerm_resource_group.rsg.name}"
+  network_security_group_id = "${azurerm_network_security_group.vault-nsg.id}"
+
+  ip_configuration {
+    name                          = "IPConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.vault-pip.id
+  }
+
+  tags {
+    environment = "dev"
+  }
+}
+
+resource "tls_private_key" "key" {
+  algorithm   = "RSA"
+}
+
+resource "null_resource" "save-key" {
+  triggers {
+    key = "${tls_private_key.key.private_key_pem}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      mkdir -p ${path.module}/.ssh
+      echo "${tls_private_key.key.private_key_pem}" > ${path.module}/.ssh/id_rsa
+      chmod 0600 ${path.module}/.ssh/id_rsa
+EOF
+  }
+}
+
+resource "azurerm_virtual_machine" "vault-vm" {
+  name                          = "vault-vm"
+  location                      = "${var.location}"
+  resource_group_name           = "${azurerm_resource_group.rsg.name}"
+  network_interface_ids         = ["${azurerm_network_interface.vault-nic.id}"]
+  vm_size                       = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "vault-demo-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "vault-demo"
+    admin_username = "azureuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = "${trimspace(tls_private_key.key.public_key_openssh)} user@vaultdemo.io"
+    }
+  }
+
+  identity = {
+    type = "SystemAssigned"
+  }
+
+  tags {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vault-extension" {
+  name                  = "vault-demo-extension"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.rsg.name}"
+  virtual_machine_name  = "${azurerm_virtual_machine.vault-vm.name}"
+  publisher             = "Microsoft.OSTCExtensions"
+  type                  = "CustomScriptForLinux"
+  type_handler_version  = "1.2"
+
+  settings             = <<SETTINGS
+    {
+      "commandToExecute": "${var.cmd_extension}",
+       "fileUris": [
+        "${var.cmd_script}"
+       ]
+    }
+SETTINGS
+}
+
+# Gets the current subscription id
+data "azurerm_subscription" "primary" {}
+
+resource "azurerm_role_assignment" "vault-demo" {
+  scope                = "${data.azurerm_subscription.primary.id}"
+  role_definition_name = "Reader"
+  principal_id         = "${lookup(azurerm_virtual_machine.vault-vm.identity[0], "principal_id")}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "location" {}
+
+variable "resource_group_name" {}
+
+variable "address_space" {}
+
+variable "sg_name" {
+  default = "vault-demo"
+}
+
+variable "subnet_prefixes" {}
+
+variable "subnet_names" {
+  default = ["azure-vault-demo-public-subnet", "azure-vault-demo-private-subnet"]
+}
+
+## Provisioning script variables
+
+variable "cmd_extension" {
+  description = "Command to be excuted by the custom script extension"
+  default     = "sh vault-install.sh"
+}
+
+variable "cmd_script" {
+  description = "Script to download which can be executed by the custom script extension"
+  default     = "https://gist.githubusercontent.com/anubhavmishra/0b6eb19f38e63bb2eb9d459fd1c53b1d/raw/696eea84b8d12cd099c283439c2c412ae13d308d/vault-install.sh"
+}
