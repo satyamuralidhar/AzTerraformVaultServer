d9c22354e237b48928c44b7b4ab8405981e4c1a1
satya <muralidhar.peddireddi@gmail.com>
2023-01-06 20:13:36 +0530

    updated provider

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rsg" {
