050a874178696b3527410848a2fc634869e9276e
satya <muralidhar.peddireddi@gmail.com>
2023-01-07 15:33:30 +0530

    ad

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "azurerm_linux_virtual_machine" "vault-vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
